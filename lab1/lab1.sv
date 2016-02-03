9b23c7780225c413710403e7520850c10856aa16
sc3973 <sc3973@micro15.ilab.columbia.edu>
2016-02-03 12:31:38 -0500

    Commented code

--- lab1/lab1.sv
+++ lab1/lab1.sv
@@ -2,8 +2,8 @@
 //
 // Spring 2014
 //
-// By: (put your name here)
-// Uni: (put your uni here)
+// By: Ayush Jain & Shivam Choudhary
+// Uni: aj2672 & sc3973
 
 module lab1(input logic       clk,
             input logic [3:0] KEY,
@@ -30,50 +30,50 @@ module controller(input logic        clk,
 		  output logic 	     we);
 
    // Replace these with your code
-//   assign a = KEY;
-//   assign din = {KEY, ~KEY};
-//   assign we = 1'b1;
-		logic flag;
+		logic flag; //flag required to change only on input.
 		logic [7:0] 			 mem [15:0];
 		always_ff @(posedge clk)
 			case (~KEY)
-				4'b0001: if(flag ==1'b0) 
+				4'b0001: if(flag ==1'b0) //KEY 0 Increments the value to be stored in Memory a
 					begin
-						a=a;
 						din =dout+8'b00000001;
 						flag=1'b1;
 						we = 1'b1;
 					end
-				4'b0010: if(flag ==1'b0) 
+				4'b0010: if(flag ==1'b0) //KEY 1 Decrements the value to be stored in Memory a
 					begin
-						a=a;
 						din =dout-8'b00000001;
 						flag=1'b1;
 						we = 1'b1;
 					end
-				4'b0100: if(flag ==1'b0) 
+				4'b0100: if(flag ==1'b0) //Key 2 Increments the memory address of a
 					begin 
 						a = a+4'b0001;
 						flag =1'b1;
 						we = 1'b0;
 					end
-				4'b1000: if(flag ==1'b0) 
+				4'b1000: if(flag ==1'b0) //Key 3 Decrements the memory address of a
 					begin 
 						a = a-4'b0001;
-						//dout = mem[a];
 						flag=1'b1;
 						we = 1'b0;
 					end
-				default: flag=1'b0;
+				4'b1100: if(flag ==1'b0)
+					begin
+						
+					end
+				default: flag=1'b0;// Default sets flag to Zero.
 			endcase			
 endmodule
 		  
 module hex7seg(input logic [3:0] a,
 	       output logic [7:0] y);
 
-   //assign y = {a,a}; // Replace this with your code
+  // Replace this with your code
 	always_comb 
 		case (~a)
+			//7 Segment output codes. B and D are small because they resemble 8 and 0
+			// respectively.
 			4'd0: 	y = 8'b00111111; //0
 			4'd1: 	y = 8'b00000110; //1
 			4'd2: 	y = 8'b01011011; //2
@@ -84,10 +84,10 @@ module hex7seg(input logic [3:0] a,
 			4'd7: 	y = 8'b00000111; //7  
 			4'd8: 	y = 8'b01111111; //8
 			4'd9: 	y = 8'b01100111; //9
-			4'd10: 	y = 8'b00111111; //A
-			4'd11: 	y = 8'b01111100; //B
+			4'd10: 	y = 8'b01110111; //A
+			4'd11: 	y = 8'b01111100; //b
 			4'd12: 	y = 8'b00111001; //C
-			4'd13: 	y = 8'b01011110; //D
+			4'd13: 	y = 8'b01011110; //d
 			4'd14: 	y = 8'b01111001; //E
 			4'd15: 	y = 8'b01110001; //F
 			default: y = 8'b00000000;
