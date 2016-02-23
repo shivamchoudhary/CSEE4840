251594ce1dfbe28d21efb9c7ac14bf1609207af4
Shivam Choudhary <shivam.07@gmail.com>
2016-02-22 22:43:19 -0500

    Update fbputchar.h

--- lab2/sc3973/fbputchar.h
+++ lab2/sc3973/fbputchar.h
@@ -10,5 +10,6 @@
 extern int fbopen(void);
 extern void fbputchar(char, int, int);
 extern void fbputs(const char *, int, int);
-
+extern void clearscreen(int startRow, int endRow);
+extern void basicview(void);
 #endif
